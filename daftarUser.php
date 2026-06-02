6bd21c746265fbde73dc12cf95a9fd8226ddf449
Waliyal Huzan <waliyalhuzan@gmail.com>
2026-06-02 12:30:35 +0700

    edit daftar user

--- daftarUser.php
+++ daftarUser.php
@@ -3,6 +3,6 @@
         <input type="text" class="auth-input" placeholder="Username">
         <input type="tel" class="auth-input" placeholder="No. WhatsApp (+62...)">
         <input type="password" class="auth-input" placeholder="Buat password (min. 8 karakter)">
-        <button class="auth-btn">Buat Akun Sekarang →</button>
+        <button class="auth-btn">Buat Akun Sekarang kalau tidak →</button>
     </div>
 </div>
